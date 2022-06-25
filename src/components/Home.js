a9c35058d98eeb44f934ba8acda8cdc41523ae3b
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-06-26 03:00:44 +0600

    almost-finish

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mx-20 mt-20'>
+            <h1 className='text-4xl font-bold'>Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gogh, Matisse, & More</h1>
+            <p className='font-semibold text-gray-600'>Multi-Day Courses ?</p>
+        </div>
+    );
+};
+
+export default Home;
